b1fc64fbc65ba780eb9154e7f63c1cf03784bd6e
bhavesh-ad <anas@wednesday.is>
2024-09-23 14:32:34 +0530

    Sentry integration

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,16 @@ import withBundleAnalyzer from "@next/bundle-analyzer"
 import createNextIntlPlugin from "next-intl/plugin"
 import withPlugins from "next-compose-plugins"
 import { env } from "./env.mjs"
+import { withSentryConfig } from "@sentry/nextjs";
 
 const withNextIntl = createNextIntlPlugin("./app/i18n/request.ts")
 
 /**
  * @type {import('next').NextConfig}
  */
-const config = withPlugins([[withBundleAnalyzer({ enabled: env.ANALYZE })]], {
+const config = withPlugins([
+  [withBundleAnalyzer({ enabled: env.ANALYZE })],
+], {
   reactStrictMode: true,
   logging: {
     fetches: {
@@ -26,4 +29,8 @@ const config = withPlugins([[withBundleAnalyzer({ enabled: env.ANALYZE })]], {
   },
 })
 
-export default withNextIntl(config)
+export default withSentryConfig(withNextIntl(config), {
+  silent: false,
+  org: "ws-9m",
+  project: "ts-next",
+})
---
+++ sentry.server.config.ts
@@ -0,0 +1,10 @@
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: process.env.SENTRY_DS_TOKEN,
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for tracing.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});

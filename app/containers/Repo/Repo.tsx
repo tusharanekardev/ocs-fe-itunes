88b3ab9297c5b17c6b1478e8b3e7cc461f70ee9e
Admin <admin@Admins-MacBook-Pro.local>
2024-10-23 14:41:24 +0530

    fix- uncomment RepoHeader and RepoBody

--- app/containers/Repo/Repo.tsx
+++ app/containers/Repo/Repo.tsx
@@ -1,8 +1,8 @@
 import React from "react"
 // import { useRecoilState } from "recoil"
 import { useTranslations } from "next-intl"
-// import RepoHeader from "./RepoHeader"
-// import RepoBody from "./RepoBody"
+import RepoHeader from "./RepoHeader"
+import RepoBody from "./RepoBody"
 
 const Repo = () => {
   const t = useTranslations()
@@ -10,8 +10,8 @@ const Repo = () => {
   return (
     <div className="container mx-auto max-w-3xl p-4">
       <h1 className="mb-4 text-2xl font-bold">{t("title")}</h1>
-      {/* <RepoHeader />
-      <RepoBody /> */}
+      <RepoHeader />
+      <RepoBody />
     </div>
   )
 }
